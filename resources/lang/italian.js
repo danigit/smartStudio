4f36d6e9fc6b49d3aecf581f8ba4e39c8adcf6bb
danigit <ds.acconto@gmail.com>
2020-09-22 11:22:06 +0100

    V 3.37.0 FULL ZONES
    - Implemented the alarm system for the full zones
    - Implemented the full zones table with the relative informations
    - Implemented the field MAX_PEOPLE with the crud operations
    - Fixed isTagAtCoords function

--- resources/lang/italian.js
+++ resources/lang/italian.js
@@ -413,6 +413,11 @@ function languageController($scope) {
         outCount: 'Numero uscite',
         noValue: 'Nessun valore',
         accessHistory: 'Storico accessi',
+        maxPeople: 'Limite affolamento',
+        fullZones: 'Zone affolate',
+        insideVsMax: 'Presenti / Totali',
+        zoneName: 'Nome zona',
+        anchorName: 'Nome ancora'
     }
 }
 
